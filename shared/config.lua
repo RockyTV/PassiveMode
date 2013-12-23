f5ff8cccaaecb7e04aef65533a5a30ccddb979c6
RockyTV <rockytvbr@gmail.com>
2013-12-23 16:23:42 -0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,9 +1,10 @@
 -- PassiveMode by RockyTV & Anzu. http://rockytv.github.io/PassiveMode
 
 -- Sets the passive mode commands to be admin only.
-onlyAdmins = true
+onlyAdmins = false
 
 -- Controls network traffic. Set to false if you don't want too much network traffic in your server.
+-- Does nothing, yet.
 minimizeTraffic = false
 
 -- Add your admins SteamID in this list (separated by commas)
